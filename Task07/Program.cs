88ce673fc6c1b1c7ec678704ae3acb86ca18b761
Venera <osmven@gmail.com>
2022-10-14 14:42:12 +0300

    Task07/

---
+++ Task07/Program.cs
@@ -0,0 +1,19 @@
+﻿/*7. Напишите программу, которая принимает 
+1. на вход трёхзначное число
+2. на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8*/
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int lastDigit = number % 10;
+    System.Console.WriteLine($"Последняя цифра числа = {lastDigit}");
+}
+else
+{
+    System.Console.WriteLine("Введите трехзначное число!");
+}
