c47ee2dd1c1aba23902db970399cf7b3191c1757
Venera <osmven@gmail.com>
2022-10-14 14:41:33 +0300

    Task03/

---
+++ Task03/Program.cs
@@ -0,0 +1,42 @@
+﻿/*3.Напишите программу,которая будет выдавать название дня недели по заданному номеру. 
+3 -> Среда 5 -> Пятница*/
+
+System.Console.WriteLine("Введите целое число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8)
+{
+    if (day == 1)
+    {
+        System.Console.WriteLine("Понедельник");
+    }
+    else if (day == 2)
+    {
+        System.Console.WriteLine("Вторник");
+    }
+    else if (day == 3)
+    {
+        System.Console.WriteLine("Среда");
+    }
+    else if (day == 4)
+    {
+        System.Console.WriteLine("Четверг");
+    }
+    else if (day == 5)
+    {
+        System.Console.WriteLine("Пятница");
+    }
+    else if (day == 6)
+    {
+        System.Console.WriteLine("Суббота");
+    }
+    else if (day == 7)
+    {
+        System.Console.WriteLine("Воскресенье");
+    }
+
+}
+else
+{
+    System.Console.WriteLine("Заданное число не соответствует дню недели");
+}
+
