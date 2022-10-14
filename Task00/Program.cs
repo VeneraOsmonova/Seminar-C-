485b17edf47f988d4c3234479ed1157ae3cb1ae8
Venera <osmven@gmail.com>
2022-10-14 14:33:32 +0300

    Task00/

---
+++ Task00/Program.cs
@@ -0,0 +1,13 @@
+﻿/*0. 
+1. на вход принимает число
+2. выдаёт его квадрат (число
+умноженное на само себя).
+Например: 4 -> 16;  -3 -> 9;   -7 -> 49*/
+
+System.Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// string str = "4";
+// string str = Console.ReadLine();
+// int number = Convert.ToInt32;
+int square = number * number;
+System.Console.WriteLine($"Kвадрат числа {number} = {square}");
