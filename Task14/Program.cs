c2e1c672c68a91b4beb7c5c074355e91fbd0dcb3
Venera <osmven@gmail.com>
2022-10-17 17:37:01 +0300

    Task14/

---
+++ Task14/Program.cs
@@ -0,0 +1,38 @@
+﻿/*14. Напишите программу, которая 
+1. принимает на вход число и
+2. проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да*/
+
+                                   //1 ВАРИАНТ (Method):
+bool DivNum(int digit, int num1, int num2)
+{
+    int result1 = digit % num1;
+    int result2 = digit % num2;
+    if (result1 == 0 && result2 == 0) return true;
+    return false;
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int number1 = 7;
+int number2 = 23;
+
+bool result = DivNum(num, number1, number2);
+if (result)
+{
+    Console.WriteLine("Кратное.");
+}
+else
+{
+    Console.WriteLine("Не кратны.");
+}
+
+
+
+
+
+
+
+
