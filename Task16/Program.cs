cbe36bc97fbae335cf7be890111813ea61b462e6
Venera <osmven@gmail.com>
2022-10-17 17:40:30 +0300

    Task16/

---
+++ Task16/Program.cs
@@ -0,0 +1,22 @@
+﻿/*16. Напишите программу, которая принимает 
+1.на вход два числа и
+2.проверяет, является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет*/
+
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+       Console.WriteLine("Нет");
+}
